6e27b99741b1885245dee40df9b5021b1e96493f
Gauthiergit <g.p220498@gmail.com>
2026-02-19 11:38:45 +0100

    font with hook

--- mobileModule03/advanced_weather_app/app/_layout.tsx
+++ mobileModule03/advanced_weather_app/app/_layout.tsx
@@ -1,6 +1,9 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
 import { useColorScheme } from "react-native";
+import { useFonts } from 'expo-font';
+import { useEffect } from "react";
+import * as SplashScreen from 'expo-splash-screen';
 
 const lightTheme = {
 	...DefaultTheme,
@@ -18,13 +21,29 @@ const darkTheme = {
 	},
 };
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
 	const colorScheme = useColorScheme();
-  return (
-	<ThemeProvider value={colorScheme === 'dark' ? darkTheme : lightTheme}>
-		<Stack>
-	        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-		</Stack>
-	</ThemeProvider>
-  )
+	const [loaded, error] = useFonts({
+	    'Nunito-Regular': require('../assets/fonts/Nunito-Regular.ttf'),
+	    'Nunito-Bold': require('../assets/fonts/Nunito-Bold.ttf')
+	});
+
+	useEffect(() => {
+		if (loaded || error) {
+		SplashScreen.hideAsync();
+		}
+	}, [loaded, error]);
+
+	if (!loaded && !error) {
+		return null;
+	}
+	return (
+		<ThemeProvider value={colorScheme === 'dark' ? darkTheme : lightTheme}>
+			<Stack>
+				<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+			</Stack>
+		</ThemeProvider>
+	)
 }
